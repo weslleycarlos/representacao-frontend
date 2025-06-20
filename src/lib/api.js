03345f242e493fa4b557373f579570121fee3e61
Weslley <weslley.unemat@gmail.com>
2025-06-19 22:12:08 -0300

    ajuste do api.js

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api';
 
 // Configuração do axios
 export const api = axios.create({
